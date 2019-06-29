4ed6a53466b7f93af14a9989b06942913a110db7
Victoria Alves <victoria.elizabethaalves@inf.ufrgs.br>
2019-06-29 19:23:52 -0300

    "Changing tabu list size"
    Co-authored-by: Christian Schmitz <christian.schmitz@uol.com.br>

--- main.py
+++ main.py
@@ -8,5 +8,6 @@ if __name__ == "__main__":
         print(f'Usage: {argv[0]} <TSPDL instance path>')
         exit(1)
     instance = parser.read_tspdl_instance(argv[1])
-    tabu = Tabu(instance, 1000, 5000, 500)
+    tabu_size = 10 if int(instance['number_of_ports']) < 50 else 1000
+    tabu = Tabu(instance, 1000, tabu_size, 500)
     tabu.run(0)
